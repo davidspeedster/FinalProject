71201dbf866a5f31854ba19643c592303672576f
Dawit Mahetemework <Dawitmahetemework@gmail.com>
2022-06-14 09:14:23 +0300

    yasg removal

--- malaria_api/urls.py
+++ malaria_api/urls.py
@@ -1,27 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from malaria_api import views
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf.urls.static import static
 from django.conf import settings
 
 app_name = 'malaria_api'
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-    authentication_classes={}
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
